8e981cb8b5498eea717b1e14d3c1d4146ddf5dc5
alan <alwat91@uga.edu>
2017-03-15 16:15:40 -0400

    Commented

--- db/seeds.js
+++ db/seeds.js
@@ -1,51 +1,64 @@
+// access api keys
 require('dotenv').config();
+// set up request-json
 var request = require('request-json');
 var client = request.createClient('http://localhost:8888/');
-
+// connect to mongoose
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/travel-app');
+// password encryptor
 var bcrypt = require('bcrypt');
-var get = require('../helpers/req.js')
-
+// models
 var City = require('../models/city');
 var List = require('../models/list');
 var User = require('../models/user');
-
+// Remove existing records
 City.remove({}, function(err){
   console.log(err);
 });
-
 List.remove({}, function(err){
   console.log(err);
 });
-
 User.remove({}, function(err){
   console.log(err);
 });
-
+// create new user
+var user1 = new User({
+  email: "a@a.com",
+  password_digest: bcrypt.hashSync("a", bcrypt.genSaltSync(10)),
+  destinations: list,
+  already_been: list
+})
+// cities to create
 var cities = ["Hong Kong, Hong Kong", "Singapore, Singapore", "Bangkok, Thailand", "London, UK", "Macau, Macau", "Kuala Lumpur, Malaysia", "Shenzhen, China", "New York City, New York", "Antalya, Turkey", "Paris, France"];
-
-var list2 = new List({
+// list to create
+var list = new List({
   description: "Top 10"
 })
-
+// loop through cities
 cities.forEach(function(name){
+  // create city object
   city = new City({
     description: name
   });
+  // get skyScanner info, then get places info
   getSkyscanner(city)
 })
 
 
 function getSkyscanner(city){
+  // request autosuggestion for city from skyscanner
   client.get(`http://partners.api.skyscanner.net/apiservices/autosuggest/v1.0/US/USD/en-US/?query=${city.description}&apiKey=${process.env.SKYSCANNER_KEY}`)
   .then(function(response) {
+    // check to see if the expected response is given
     if (response.body.Places[0]) {
+      // set the skyscanner_id
       city.skyscanner_id = response.body.Places[0].CityId;
     }
     else {
       console.log(response.body);
     }
+    // After adding skyscanner_id, get the places id
     getPlaces(city);
   })
   .catch(function(res){
@@ -54,71 +67,49 @@ function getSkyscanner(city){
 }
 
 function getPlaces(city){
+  // perform places search for city
   client.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?type=city&query=${city.description}&key=${process.env.PLACES_KEY}`)
   .then(function(res){
-    city.places_id = res.body.results[0].place_id;
-    city.location = [res.body.results[0].geometry.location.lat, res.body.results[0].geometry.location.lng];
-    getMaps(city);
-  })
+      // check to see if the results are as expected
+      if(res.body.results){
+        // set the places_id of the city
+        city.places_id = res.body.results[0].place_id;
+        // get the location of the city
+        city.location = [res.body.results[0].geometry.location.lat, res.body.results[0].geometry.location.lng];
+        // save the city
+        city.save(function(res){
+          console.log(city, "created");
+          list.cities.push(city);
+          // if all the cities have been saved, continue and save the users and the lists
+          if (list.cities.length = cities.length) {
+            saveAll();
+          }
+        })
+      }
+      else {
+        console.log(res);
+      }
+    })
   .catch(function(res){
     console.log(res);
   });
 }
 
-function getMaps(city){
-  console.log(city);
+
+function saveAll(){
+
+  user1.save(function(err){
+    if (err) {
+      console.log(err);
+    }
+    console.log("User created!");
+  })
+
+  list.save(function(err){
+    if (err) {
+      console.log(err);
+    }
+    console.log("List created!");
+  })
+
 }
-
-var nyc = new City({
-  description: "New York, New York",
-  places_id: "ChIJOwg_06VPwokRYv534QaPC8g",
-  skyscanner_id: "NYCA-sky",
-  location: [40.7128, -74.0059]
-});
-
-var dc = new City({
-  description: "Washington, D.C.",
-  skyscanner_id: "WASA-sky",
-  places_id: "ChIJW-T2Wt7Gt4kRKl2I1CJFUsI",
-  location: [38.9072, -77.0369]
-})
-
-var list = new List({
-  description: "Murica",
-  cities: [dc, nyc]
-})
-
-var user1 = new User({
-  email: "a@a.com",
-  password_digest: bcrypt.hashSync("a", bcrypt.genSaltSync(10)),
-  destinations: list,
-  already_been: list
-})
-
-user1.save(function(err){
-  if (err) {
-    console.log(err);
-  }
-  console.log("User created!");
-})
-
-list.save(function(err){
-  if (err) {
-    console.log(err);
-  }
-  console.log("List created!");
-})
-
-nyc.save(function(err){
-  if (err) {
-    console.log(err);
-  }
-  console.log("NYC created!");
-})
-
-dc.save(function(err){
-  if (err) {
-    console.log(err);
-  }
-  console.log("DC created!");
-})
