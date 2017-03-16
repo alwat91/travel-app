5a748b66ac95cd7dd1bcb1d4de5e989f7280c865
alan <alwat91@uga.edu>
2017-03-16 12:47:47 -0400

    Added section for maps

--- public/scripts/controllers/destinations_controller.js
+++ public/scripts/controllers/destinations_controller.js
@@ -1,12 +1,18 @@
-function DestinationsController($http){
+function DestinationsController($http, $state){
   var self = this;
 
   function getCities(){
     $http.get('/destinations')
     .then(function(res){
-      console.log(res);
+      self.cities = res.data.cities;
     })
   }
   getCities();
   self.getCities = getCities;
+
+  function randomTrip(origin){
+    self.origin = JSON.parse(origin);
+    
+  }
+  self.randomTrip = randomTrip
 }
